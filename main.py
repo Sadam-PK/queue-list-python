3c245938425ebbece41e84b9826fe36c1155cef2
sadamccr <sadamccr@gmail.com>
2023-02-07 12:39:53 +0500

    queue in reverse order

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ print(queue)
 
 queue.pop(0)
 print(queue)
+
+# # reverse queue is achieved with insert at zero and normal pop
+
+
+# queue2 = []
+# queue2.insert(0, 10)
+# queue2.insert(0, 20)
+# queue2.insert(0, 30)
+# print(queue2)
+#
+# queue2.pop()
+# print(queue2)
