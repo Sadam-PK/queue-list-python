52e1bfed9183af3c45822581aa2ff0a72168d959
sadamccr <sadamccr@gmail.com>
2023-02-07 13:42:52 +0500

    queue done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-queue = []
-queue.append(10)
-queue.append(20)
-queue.append(30)
-
-print(queue)
-
-queue.pop(0)
-print(queue)
+# queue = []
+# queue.append(10)
+# queue.append(20)
+# queue.append(30)
+#
+# print(queue)
+#
+# queue.pop(0)
+# print(queue)
 
 # # reverse queue is achieved with insert at zero and normal pop
 
@@ -19,3 +19,33 @@ print(queue)
 #
 # queue2.pop()
 # print(queue2)
+
+queue = []
+
+
+def enqueue():
+    element = input('Enter a value to add to queue')
+    queue.append(element)
+    print(f'element added to queue = {element}')
+    print(queue)
+
+
+def dequeue():
+    if not queue:
+        print('Queue is empty!')
+        return
+    element = queue.pop(0)
+    print(f'element removed from queue = {element}')
+    print(queue)
+
+
+while True:
+    print('Press 1 for adding to queue')
+    print('Press 2 for removing from queue')
+    choice = int(input())
+    if choice == 1:
+        enqueue()
+    elif choice == 2:
+        dequeue()
+    else:
+        print('Invalid option')
